05b01e0fa9da34939cd78078ed4f1c0b1fb03c84
PriorityFixa <priorityfixa@gmail.com>
2026-08-20 09:19:30 +0300

    Create business.js

---
+++ configuration/business.js
@@ -0,0 +1,15 @@
+const BUSINESS_CONFIG = {
+    name: "PriorityFixa Commerce",
+    tagline: "Simple Commerce for Growing Businesses",
+
+    contact: {
+        email: "",
+        phone: ""
+    },
+
+    location: {
+        country: "Kenya",
+        currency: "KES",
+        currencySymbol: "KSh"
+    }
+};
