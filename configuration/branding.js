e5685579943c5f2a6e24adf56363c4fb9a52e0d2
PriorityFixa <priorityfixa@gmail.com>
2026-08-20 09:21:28 +0300

    Create branding.js

---
+++ configuration/branding.js
@@ -0,0 +1,10 @@
+const BRAND_CONFIG = {
+    logo: "",
+    
+    colors: {
+        primary: "#17140F",
+        secondary: "#C9A84C",
+        background: "#FFFFFF",
+        text: "#17140F"
+    }
+};
